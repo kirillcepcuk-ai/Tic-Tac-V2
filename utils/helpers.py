d7e6f0561b01cb5cefb6623bb718901de0732d57
Ilya | night reaper 🛠️ <kirillcepcuk@gmail.com>
2026-07-03 17:01:53 +0300

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,17 @@ from typing import List, Optional, Union
 from config import WIN_COMBINATIONS # type: ignore
 
 def check_winner(board: List[str]) -> Optional[Union[str, None]]:
+    """
+    Проверить, есть ли победитель на доске.
+
+    Args:
+        board: Список из 9 элементов ('X', 'O', ' ')
+
+    Returns:
+        'X' или 'O' если есть победитель,
+        'tie' если ничья,
+        None если игра продолжается
+    """
     for a, b, c in WIN_COMBINATIONS:
         if board[a] == board[b] == board[c] and board[a] != " ":
             return board[a]
