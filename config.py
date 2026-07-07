eb2e432464874efae284fdbf580983a89dc30f29
Ilya | night reaper 🛠️ <kirillcepcuk@gmail.com>
2026-07-08 01:45:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
-from dotenv import load_dotenv # type: ignore
+from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 
 WIN_COMBINATIONS = [
     [0,1,2], [3,4,5], [6,7,8],
